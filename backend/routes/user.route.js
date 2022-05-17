5e3118f106036badf19669edb6dc0e72a43c81f9
Younes Lahouiti <loller763@gmail.com>
2022-05-17 08:48:48 +0200

    Initialisation d'Angular pour le Front-End (Uniuement initialisation, faut d'abord finir la Back-End)

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,3 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+
+router.post("/signup", email, password, userController.signup);
+router.post("/login", userController.login);
+
+module.exports = router;
