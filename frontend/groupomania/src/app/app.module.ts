82f9b0500f5977db972af27325997a0733749b25
Younes Lahouiti <loller763@gmail.com>
2022-06-11 13:52:02 +0200

    Tentative d'ajout de reactive forms à la place des templat forms (fail)

--- frontend/groupomania/src/app/app.module.ts
+++ frontend/groupomania/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,9 @@ import { LandingConnexionPageComponent } from './landing-connexion-page/landing-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/groupomania/src/app/landing-connexion-page/landing-connexion-page.component.ts
+++ frontend/groupomania/src/app/landing-connexion-page/landing-connexion-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-connexion-page',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-connexion-page.component.scss']
 })
 export class LandingConnexionPageComponent implements OnInit {
+  signupForm!:FormGroup;
+  
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //Function onSubmit that will show in the console the value of the email inputted by the user
+  onSubmit(form: NgForm): void{ 
+    console.log(form.value)
   }
 
 }
