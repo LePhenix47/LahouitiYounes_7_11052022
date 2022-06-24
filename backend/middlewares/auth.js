cd51fdc4567a23044506f49ee706ebf62e359060
Younes Lahouiti <loller763@gmail.com>
2022-06-24 14:54:48 +0200

    Rechangement du middleware d'authentification → mis comme avant

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -20,42 +20,13 @@ module.exports = (req, res, next) => {
         console.log("REQUEST AUTH = ↓");
         console.log(req.auth);
         console.log("↑");
-        let isUserAdmin = false;
-        User.findAll({
-                //SELECT moderator FROM users WHERE moderator = true AND user_id = [userID du token]
-                attributes: ["moderator"],
-                where: {
-                    moderator: true,
-                    user_id: userId,
-                },
-            })
-            .then((admin) => {
-                console.log(admin);
-                console.log("AMDIN[0] \n → ", admin[0]);
-                console.log("AMDIN[0].moderator \n → ", admin[0].moderator);
-                const mod =
-                    admin[0].moderator !== undefined ? admin[0].moderator : false;
-                if (mod) {
-                    console.log("User with userId of " + userId + " is a moderator");
-                    isUserAdmin = true;
-                }
-            })
-            .catch((findAdminError) => {
-                console.log(findAdminError);
-                res
-                    .status(500)
-                    .json({ message: "Erreur lors de la vérification de l'utilisateur" });
-            });
-        if (!isUserAdmin) {
-            console.log("User isn't admin");
-            if (req.body.user_id && req.body.user_id !== userId) {
-                throw (
-                    "Invalid User ID\nExpected value of user_id of " +
-                    userId +
-                    ", instead got userId from body request: " +
-                    req.body.user_id
-                );
-            }
+        if (req.body.user_id && req.body.user_id !== userId) {
+            throw (
+                "Invalid User ID\nExpected value of user_id of " +
+                userId +
+                ", instead got userId from body request: " +
+                req.body.user_id
+            );
         } else {
             next();
         }
